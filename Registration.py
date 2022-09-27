e19feeae37b4c823aa38c7d013b82a406e34cb1f
HarrisonA1 <harrisonallen53@gmail.com>
2022-09-27 11:11:54 -0500

    Created a Python and HTML file for registration. Need to implement actual code within them. Password is currently exposed, so we might want to look at an alternative security mechanism.

---
+++ Registration.py
@@ -0,0 +1,4 @@
+import pyodbc
+
+connection = pyodbc.connect('Driver={CData ODBC Driver for MySQL};' 'User=u6hluesb8rni;' 'Password=N0NameTeam;' 'Database=NonameStoryDB;' 'Server=localhost;' 'Port=3306;')
+
