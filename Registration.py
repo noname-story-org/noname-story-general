f345365b935ee69102d70b983f7de40b65a37bc4
HarrisonA1 <harrisonallen53@gmail.com>
2022-10-03 13:03:17 -0500

    I started work on the background logic for the registration system, though it's still just a framework and I will have NO idea if it will work until it's on the webserver, because the only way to connect to the DB is with the localhost parameter

--- Registration.py
+++ Registration.py
@@ -1,9 +1,36 @@
 import pyodbc
 import os
+import cgi, cgitb
+from io import StringIO
 
+#Opens cgi.FieldStorage()
+form = cgi.FieldStorage()
+
+#Imports credentials from environment variables
 username = os.getenv('USER_NAME')
 password = os.getenv('PASSWORD')
 
-connection = pyodbc.connect('Driver={SQL Server};' 'Server=noname-story.com;' 'Database=NonameStoryDB;' 'UID=username;' 'PWD=password;')
+#Query text templates
+checkIsEmpty = "if exists(select * from userData)"
+
+#Establishes a connection to the database
+connection = pyodbc.connect('Driver={SQL Server};' 'Server=localhost;' 'Database=NonameStoryDB;' 'UID=username;' 'PWD=password;')
 cursor = connection.cursor()
-cursor.execute()
+
+#Test if database connection works
+#cursor.execute('SELECT * FROM userData')
+
+#Retrieves information from Registration page
+user_name = form.getvalue('user_name')
+user_password = form.getvalue('user_password')
+
+user_query = "SELECT COUNT(*) FROM userData WHERE userName = " + str(user_name) + ";"
+userName_Count = cursor.execute(user_query)
+
+#Checks to see if retrieved information is already in database (Also checks if DB is empty)
+if(userName_Count > 0) : 
+    
+else : 
+    
+    #Throw error and reset if User is already in database
+    #If user is not in database, insert new user data with an ID num equal to max + 1
