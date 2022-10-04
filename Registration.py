7369a4a5252386fdc12bd9980895ac6aac4d79b4
HarrisonA1 <harrisonallen53@gmail.com>
2022-10-04 10:14:49 -0500

    Update to Registration.py
    I added a lot more functionality to retrieve and compare different sets of data from the Database and an HTML form (Yet to be implemented)

--- Registration.py
+++ Registration.py
@@ -1,6 +1,7 @@
 import pyodbc
 import os
 import cgi, cgitb
+import datetime
 from io import StringIO
 
 #Opens cgi.FieldStorage()
@@ -23,14 +24,22 @@ cursor = connection.cursor()
 #Retrieves information from Registration page
 user_name = form.getvalue('user_name')
 user_password = form.getvalue('user_password')
+user_email = form.getvalue('user_email')
 
+#Retrieves how many instances of the inputted username exists (if any) so that we know if we should tell the user to post a new  username
 user_query = "SELECT COUNT(*) FROM userData WHERE userName = " + str(user_name) + ";"
-userName_Count = cursor.execute(user_query)
+userName_Count = int(cursor.execute(user_query))
+
+#Creates the next available userID
+user_id_query = "SELECT MAX(userID) FROM userData;"
+user_id = int(cursor.execute(user_id_query)) + 1
+
+#Gets the current date/time so that we can input the joinDate and initial loginDate
+datetime_object = datetime.datetime.now()
 
 #Checks to see if retrieved information is already in database (Also checks if DB is empty)
 if(userName_Count > 0) : 
+    #Throw error and reset if User is already in database
     
 else : 
-    
-    #Throw error and reset if User is already in database
     #If user is not in database, insert new user data with an ID num equal to max + 1
